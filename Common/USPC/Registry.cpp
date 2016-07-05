ff8500ee3f69a0e150e8645fc65e906dbffef62f
Alcoa1 <Alcoa1@mail.ru>
2016-07-05 12:32:55 +0400

    реестр

--- Common/USPC/Registry.cpp
+++ Common/USPC/Registry.cpp
@@ -3,17 +3,18 @@
 
 RegistryPathLogFile::RegistryPathLogFile()
 {
-	RegOpenKeyEx (HKEY_CURRENT_USER, 
-	L"Software\\Microsoft\\AuthCookies\\Live\\Default\\DIDC",NULL,KEY_READ,&hKey);
+	RegOpenKeyEx (HKEY_LOCAL_MACHINE, 
+	L"SOFTWARE\\Wow6432Node\\USPC7100\\Paths",NULL,KEY_READ,&hKey);
 	DWORD size = 1024;
 	RegQueryValueEx(
 		hKey,
-		L"URL",
+		L"Applications",
 		NULL,
 		NULL,
 		(LPBYTE)buffer,
 		&size
 		);
+	wcscat(buffer, L"USPC7100log.txt");
 }
 RegistryPathLogFile::~RegistryPathLogFile()
 {
