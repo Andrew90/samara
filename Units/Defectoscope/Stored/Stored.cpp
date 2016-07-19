da69562a8730e8bcfed9a94644f7642b2d594ed4
Alcoa1 <Alcoa1@mail.ru>
2016-07-19 08:51:40 +0400

    scope_velocity

--- Units/Defectoscope/Stored/Stored.cpp
+++ Units/Defectoscope/Stored/Stored.cpp
@@ -47,6 +47,7 @@ namespace Stored
 			 fwrite(&longData.currentOffsetFrames, sizeof(int), 1, f);
 		     fwrite(&thicknessData.currentOffsetFrames, sizeof(int), 1, f);
 			 fwrite(&compute.lengthTube, sizeof(int), 1, f);
+			 fwrite(&thicknessData.scope_velocity, sizeof(thicknessData.scope_velocity), 1, f);
 
 			 __Store__(crossData.ascanBuffer    , crossData.currentOffsetFrames, f);
 			 __Store__(longData.ascanBuffer     , longData.currentOffsetFrames, f);
@@ -78,6 +79,7 @@ namespace Stored
 			 fread(&longData.currentOffsetFrames, sizeof(int), 1, f);
 		     fread(&thicknessData.currentOffsetFrames, sizeof(int), 1, f);
 			 fread(&compute.lengthTube, sizeof(int), 1, f);
+			 fread(&thicknessData.scope_velocity, sizeof(thicknessData.scope_velocity), 1, f);
 
 			 __Load__(crossData.ascanBuffer, crossData.currentOffsetFrames, f);
 			 __Load__(longData.ascanBuffer, longData.currentOffsetFrames, f);
