90a018b2f07e6bd587b62b5ad702a43eadb7f845
AndreyNoutBook <andrey@uran-ndt.ru>
2016-07-27 09:17:28 +0500

    поменял текст

--- Units/Defectoscope/Automat/AutomatAdditional.cpp
+++ Units/Defectoscope/Automat/AutomatAdditional.cpp
@@ -13,7 +13,7 @@ namespace AutomatAdditional
 			unsigned referenceOffset = Singleton<AdditionalSettingsTable>::Instance().items.get<ReferenceOffset1>().value;
 			double speed = 1000.0 * referenceOffset / t;
 			wchar_t buf[128];
-			wsprintf(buf, L"Скорость движения трубы %s мм/сек"
+			wsprintf(buf, L"Скорость %s мм/сек"
 				, Wchar_from<double, 4>(speed)()
 				);
 		    app.MainWindowBottomLabel(App::speed_bottom_label, buf);
