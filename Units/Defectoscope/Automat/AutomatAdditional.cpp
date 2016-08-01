8c947147b8c8c4635350d47cf2426d9fa1ac9db6
AndreyNoutBook <andrey@uran-ndt.ru>
2016-08-01 11:21:35 +0500

    в начале цикла скорость удаляеися из строки состояния

--- Units/Defectoscope/Automat/AutomatAdditional.cpp
+++ Units/Defectoscope/Automat/AutomatAdditional.cpp
@@ -8,16 +8,17 @@ namespace AutomatAdditional
 {
 	void ComputeSpeed(unsigned t)
 	{
+		wchar_t buf[128];
+		buf[0] = 0;
 		if(0 != t)
 		{
 			unsigned referenceOffset = Singleton<AdditionalSettingsTable>::Instance().items.get<ReferenceOffset1>().value;
 			double speed = 1000.0 * referenceOffset / t;
-			wchar_t buf[128];
 			wsprintf(buf, L"Скорость %s мм/сек"
 				, Wchar_from<double, 4>(speed)()
 				);
-		    app.MainWindowBottomLabel(App::speed_bottom_label, buf);
 		}
+		app.MainWindowBottomLabel(App::speed_bottom_label, buf);
 	}
 
 	void SetToBottomLabel()
