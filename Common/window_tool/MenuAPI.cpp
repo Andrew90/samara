96d2f6a970a6bbb0e9a5b8f35bc1c4a4927ec144
andreyV512 <drewan@rambler.ru>
2016-09-15 10:02:08 +0500

    MenuApi добавил try catch

--- Common/window_tool/MenuAPI.cpp
+++ Common/window_tool/MenuAPI.cpp
@@ -6,37 +6,45 @@
 //---------------------------------------------------------------------------
 void EventDo(TCommand &m)
 {
-	if(m.hControl)
+	try
 	{
-		if(m.id)
+		if(m.hControl)
 		{
-			TBBUTTONINFO button_info;
-			button_info.cbSize = sizeof(button_info);
-			button_info.dwMask = TBIF_LPARAM;
-			SendMessage(m.hControl, (UINT) TB_GETBUTTONINFO, m.id , (LPARAM)&button_info);
-			if(!IsBadReadPtr((void *)button_info.lParam, sizeof(void *)))
+			if(m.id)
+			{
+				TBBUTTONINFO button_info;
+				button_info.cbSize = sizeof(button_info);
+				button_info.dwMask = TBIF_LPARAM;
+				SendMessage(m.hControl, (UINT) TB_GETBUTTONINFO, m.id , (LPARAM)&button_info);
+				//if(!IsBadReadPtr((void *)button_info.lParam, sizeof(void *)))
 				((void (__cdecl *)(HWND))(button_info.lParam))(m.hwnd);
+			}
+			else if(TEvent *x = (TEvent *)GetWindowLongPtr(m.hControl, GWLP_USERDATA))
+			{
+				x->Do(m);
+			}
 		}
-		else if(TEvent *x = (TEvent *)GetWindowLongPtr(m.hControl, GWLP_USERDATA))
+		else
 		{
-			x->Do(m);
-		}
-	}
-	else
-	{
-		MENUITEMINFO mii;
-		mii.cbSize = sizeof(MENUITEMINFO);
-		mii.fMask = MIIM_DATA;
-		if(GetMenuItemInfo(GetMenu(m.hwnd), m.id, false, &mii))
-		{
-			if(!IsBadReadPtr((void *)mii.dwItemData, sizeof(void *)))
+			MENUITEMINFO mii;
+			mii.cbSize = sizeof(MENUITEMINFO);
+			mii.fMask = MIIM_DATA;
+			if(GetMenuItemInfo(GetMenu(m.hwnd), m.id, false, &mii))
+			{
+				//if(!IsBadReadPtr((void *)mii.dwItemData, sizeof(void *)))
 				((void (__cdecl *)(HWND))(mii.dwItemData))(m.hwnd);
+			}
 		}
 	}
+	catch(...){}
 }
 
 void EventDo(TNotify &m)
 {
-	TEvent *x = (TEvent *)GetWindowLongPtr(m.pnmh->hwndFrom, GWLP_USERDATA);
-	if(NULL != x) x->Do(m);
+	try
+	{
+		TEvent *x = (TEvent *)GetWindowLongPtr(m.pnmh->hwndFrom, GWLP_USERDATA);
+		if(NULL != x) x->Do(m);
+	}
+	catch(...){}
 }
