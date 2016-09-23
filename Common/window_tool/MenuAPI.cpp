2d2d748f4f33fdb0179e9e90f4bfaa9fab402ab0
Andrey <andrey@uran-ndt.ru>
2016-09-23 11:03:46 +0500

    Zip

--- Common/window_tool/MenuAPI.cpp
+++ Common/window_tool/MenuAPI.cpp
@@ -12,27 +12,27 @@ void EventDo(TCommand &m)
 		{
 			if(m.id)
 			{
-				TBBUTTONINFO button_info;
+				TBBUTTONINFO button_info = {};
 				button_info.cbSize = sizeof(button_info);
 				button_info.dwMask = TBIF_LPARAM;
 				SendMessage(m.hControl, (UINT) TB_GETBUTTONINFO, m.id , (LPARAM)&button_info);
 				//if(!IsBadReadPtr((void *)button_info.lParam, sizeof(void *)))
-				((void (__cdecl *)(HWND))(button_info.lParam))(m.hwnd);
+				if(NULL != button_info.lParam)((void (__cdecl *)(HWND))(button_info.lParam))(m.hwnd);
 			}
 			else if(TEvent *x = (TEvent *)GetWindowLongPtr(m.hControl, GWLP_USERDATA))
 			{
-				x->Do(m);
+				if(NULL != x)x->Do(m);
 			}
 		}
 		else
 		{
-			MENUITEMINFO mii;
+			MENUITEMINFO mii = {};
 			mii.cbSize = sizeof(MENUITEMINFO);
 			mii.fMask = MIIM_DATA;
 			if(GetMenuItemInfo(GetMenu(m.hwnd), m.id, false, &mii))
 			{
 				//if(!IsBadReadPtr((void *)mii.dwItemData, sizeof(void *)))
-				((void (__cdecl *)(HWND))(mii.dwItemData))(m.hwnd);
+				if(NULL != mii.dwItemData)((void (__cdecl *)(HWND))(mii.dwItemData))(m.hwnd);
 			}
 		}
 	}
