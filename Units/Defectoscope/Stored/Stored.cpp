6757c651caa7866edfedae79b44cbb0959f5d01b
AndreyNoutBook <andrey@uran-ndt.ru>
2016-10-31 16:33:32 +0500

    ограничение на количество сохранённых файлов

--- Units/Defectoscope/Stored/Stored.cpp
+++ Units/Defectoscope/Stored/Stored.cpp
@@ -38,70 +38,70 @@ namespace Stored
 	}
 	void __Store__(USPC7100_ASCANDATAHEADER *d, int count, FILE *f)
 	{
-	    	fwrite(d, sizeof(USPC7100_ASCANDATAHEADER), count, f);
+		fwrite(d, sizeof(USPC7100_ASCANDATAHEADER), count, f);
 	}
 	void DataToFile(wchar_t *path)
 	{
-		 FILE *f = _wfopen(path, L"wb");
-		 if(NULL != f)
-		 {
-			 Version::SaveToFile(f);
-			 fwrite(&crossData.currentOffsetFrames, sizeof(int), 1, f);
-			 fwrite(&longData.currentOffsetFrames, sizeof(int), 1, f);
-		     fwrite(&thicknessData.currentOffsetFrames, sizeof(int), 1, f);
-			 fwrite(&compute.lengthTube, sizeof(int), 1, f);
-			 __Store__(crossData.ascanBuffer    , crossData.currentOffsetFrames, f);
-			 __Store__(longData.ascanBuffer     , longData.currentOffsetFrames, f);
-			 __Store__(thicknessData.ascanBuffer, thicknessData.currentOffsetFrames, f);
+		FILE *f = _wfopen(path, L"wb");
+		if(NULL != f)
+		{
+			Version::SaveToFile(f);
+			fwrite(&crossData.currentOffsetFrames, sizeof(int), 1, f);
+			fwrite(&longData.currentOffsetFrames, sizeof(int), 1, f);
+			fwrite(&thicknessData.currentOffsetFrames, sizeof(int), 1, f);
+			fwrite(&compute.lengthTube, sizeof(int), 1, f);
+			__Store__(crossData.ascanBuffer    , crossData.currentOffsetFrames, f);
+			__Store__(longData.ascanBuffer     , longData.currentOffsetFrames, f);
+			__Store__(thicknessData.ascanBuffer, thicknessData.currentOffsetFrames, f);
 
-			 fclose(f);			
-		 }
+			fclose(f);			
+		}
 	}
 
 	void __Load__(USPC7100_ASCANDATAHEADER *d, int count, FILE *f)
 	{
-	    	fread(d, sizeof(USPC7100_ASCANDATAHEADER), count, f);
+		fread(d, sizeof(USPC7100_ASCANDATAHEADER), count, f);
 	}
 
 	bool DataFromFile(wchar_t *path)
 	{
-		 FILE *f = _wfopen(path, L"rb");
-		 if(NULL != f)
-		 {
-			 fread(&crossData.currentOffsetFrames, sizeof(int), 1, f);
-			 if(Version::LoadFromFile(crossData.currentOffsetFrames, f))
-			 {
-				  fread(&crossData.currentOffsetFrames, sizeof(int), 1, f);
-			 }
-			 fread(&longData.currentOffsetFrames, sizeof(int), 1, f);
-		     fread(&thicknessData.currentOffsetFrames, sizeof(int), 1, f);
-			 if((App::count_frames < crossData.currentOffsetFrames
-				 || App::count_frames < longData.currentOffsetFrames
-				 || App::count_frames < thicknessData.currentOffsetFrames
-				 )
-				 && crossData.currentOffsetFrames > 0
-				 &&	longData.currentOffsetFrames > 0
-				 &&	thicknessData.currentOffsetFrames > 0
-				 )
-			 {
-				 crossData.currentOffsetFrames = 0;
-				 longData.currentOffsetFrames = 0;
-				 thicknessData.currentOffsetFrames = 0;
-				 compute.lengthTube = 0;
+		FILE *f = _wfopen(path, L"rb");
+		if(NULL != f)
+		{
+			fread(&crossData.currentOffsetFrames, sizeof(int), 1, f);
+			if(Version::LoadFromFile(crossData.currentOffsetFrames, f))
+			{
+				fread(&crossData.currentOffsetFrames, sizeof(int), 1, f);
+			}
+			fread(&longData.currentOffsetFrames, sizeof(int), 1, f);
+			fread(&thicknessData.currentOffsetFrames, sizeof(int), 1, f);
+			if((App::count_frames < crossData.currentOffsetFrames
+				|| App::count_frames < longData.currentOffsetFrames
+				|| App::count_frames < thicknessData.currentOffsetFrames
+				)
+				&& crossData.currentOffsetFrames > 0
+				&&	longData.currentOffsetFrames > 0
+				&&	thicknessData.currentOffsetFrames > 0
+				)
+			{
+				crossData.currentOffsetFrames = 0;
+				longData.currentOffsetFrames = 0;
+				thicknessData.currentOffsetFrames = 0;
+				compute.lengthTube = 0;
 
-				 MessageBox(App::MainWindowHWND(), L"Файл имеет неизвесный формат", L"Ошибка !!!", MB_ICONERROR);
-				 fclose(f);
-				 return false;
-			 }
-			 fread(&compute.lengthTube, sizeof(int), 1, f);
-			 __Load__(crossData.ascanBuffer, crossData.currentOffsetFrames, f);
-			 __Load__(longData.ascanBuffer, longData.currentOffsetFrames, f);
-			 __Load__(thicknessData.ascanBuffer, thicknessData.currentOffsetFrames, f);
+				MessageBox(App::MainWindowHWND(), L"Файл имеет неизвесный формат", L"Ошибка !!!", MB_ICONERROR);
+				fclose(f);
+				return false;
+			}
+			fread(&compute.lengthTube, sizeof(int), 1, f);
+			__Load__(crossData.ascanBuffer, crossData.currentOffsetFrames, f);
+			__Load__(longData.ascanBuffer, longData.currentOffsetFrames, f);
+			__Load__(thicknessData.ascanBuffer, thicknessData.currentOffsetFrames, f);
 
-			 fclose(f);
-			 return true;
-		 }
-		 return false;
+			fclose(f);
+			return true;
+		}
+		return false;
 	}
 
 	template<class O, class P>struct __stored__{void operator()(O *, P *){}};
@@ -153,7 +153,7 @@ namespace Stored
 		memmove(t.items.get<Status>().value, unit.commonStatus, sizeof(t.items.get<Status>().value));
 
 		Insert_Into<StoredMeshureTable>(t, base).Execute();
-        unsigned id = 0;
+		unsigned id = 0;
 		CMD(base).CommandText(
 			L"SELECT max([ID]) as ID_LAST FROM [StoredBase].[dbo].[StoredMeshureTable]"
 			).GetValue(L"ID_LAST", id);
@@ -204,14 +204,14 @@ namespace Stored
 	bool Do()
 	{
 		COleDateTime tme = COleDateTime::GetCurrentTime();
-		
+
 		wchar_t path[1024];
 		GetModuleFileName(0, path, dimention_of(path));
 		PathRemoveFileSpec(path);
 		int len = wcslen(path);
 
 		StoredBase parameters;
-		
+
 		CExpressBase base(
 			parameters.name()
 			, CreateDataBase<StoredBase::type_list, NullType, MSsql>()
@@ -273,132 +273,136 @@ namespace Stored
 	{
 		wchar_t path[1024];
 	};
-template<class Table, class Data>struct __list__
-{
-	bool operator()(unsigned id, Table &t, Data &d)
-	{		
-		Path(d.offsPath, t.items.get<Date_Time>().value);
-		DeleteFile(d.path);
-		wcscat(d.path, L".bz2");
-		DeleteFile(d.path);
-		return false;
-	}
-};
+	template<class Table, class Data>struct __list__
+	{
+		bool operator()(unsigned id, Table &t, Data &d)
+		{		
+			Path(d.offsPath, t.items.get<Date_Time>().value);
+			DeleteFile(d.path);
+			wcscat(d.path, L".bz2");
+			DeleteFile(d.path);
+			return false;
+		}
+	};
 #if 0
-void DeleteLast::operator()(CBase &b, wchar_t *path_, wchar_t *offsPath_)
-{
-	path = path_;
-	offsPath= offsPath_;
-	base = &b;
-	if(count > 10000)
+	void DeleteLast::operator()(CBase &b, wchar_t *path_, wchar_t *offsPath_)
 	{
-		COleDateTime tme;
-		CMD(b).CommandText(
-			L"SELECT max([Date_Time]) as TME FROM [StoredBase].[dbo].[TubeTable]"\
-			L"WHERE [Date_Time] "\
-			L"IN (SELECT TOP(100)[Date_Time] FROM [StoredBase].[dbo].[TubeTable] ORDER BY [Date_Time] ASC)"
-			).GetValue(L"TME", tme);
-		Select<TubesTable>(b).op<Date_Time>(L"<", tme).ExecuteLoop<__list__>(*this);
+		path = path_;
+		offsPath= offsPath_;
+		base = &b;
+		if(count > 10000)
+		{
+			COleDateTime tme;
+			CMD(b).CommandText(
+				L"SELECT max([Date_Time]) as TME FROM [StoredBase].[dbo].[TubesTable]"\
+				L"WHERE [Date_Time] "\
+				L"IN (SELECT TOP(100)[Date_Time] FROM [StoredBase].[dbo].[TubesTable] ORDER BY [Date_Time] ASC)"
+				).GetValue(L"TME", tme);
+			Select<TubesTable>(b).op<Date_Time>(L"<", tme).ExecuteLoop<__list__>(*this);
 
-		//unsigned id = 0;
-		//CMD(b).CommandText(
-		//	L"SELECT ID FROM [StoredBase].[dbo].[TubeTable] WHERE Date_Time=?"
-		//	).Param(tme).GetValue(L"ID", id);
-		//
-		//CMD(b).CommandText(
-		//	L"DELETE FROM [StoredBase].[dbo].[StoredMeshureTable] WHERE Tube<?"
-		//	).Param(id).Execute();
+			//unsigned id = 0;
+			//CMD(b).CommandText(
+			//	L"SELECT ID FROM [StoredBase].[dbo].[TubesTable] WHERE Date_Time=?"
+			//	).Param(tme).GetValue(L"ID", id);
+			//
+			//CMD(b).CommandText(
+			//	L"DELETE FROM [StoredBase].[dbo].[StoredMeshureTable] WHERE Tube<?"
+			//	).Param(id).Execute();
 
-		CMD(b).CommandText(
-			L"DELETE FROM [StoredBase].[dbo].[StoredMeshureTable] WHERE Tube IN(SELECT ID FROM [StoredBase].[dbo].[TubeTable] WHERE Date_Time<?)"
-			).Param(tme).Execute();
+			CMD(b).CommandText(
+				L"DELETE FROM [StoredBase].[dbo].[StoredMeshureTable] WHERE Tube IN(SELECT ID FROM [StoredBase].[dbo].[TubesTable] WHERE Date_Time<?)"
+				).Param(tme).Execute();
 
-		CMD(b).CommandText(
-			L"DELETE FROM [StoredBase].[dbo].[ProtocolsTable] WHERE ID IN(SELECT IDProtocolsTable FROM [StoredBase].[dbo].[TubeTable] WHERE Date_Time<?)"
-			).Param(tme).Execute();
+			CMD(b).CommandText(
+				L"DELETE FROM [StoredBase].[dbo].[ProtocolsTable] WHERE ID IN(SELECT IDProtocolsTable FROM [StoredBase].[dbo].[TubesTable] WHERE Date_Time<?)"
+				).Param(tme).Execute();
 
-		CMD(b).CommandText(
-			L"DELETE FROM [StoredBase].[dbo].[TubeTable] WHERE Date_Time<?"
-			).Param(tme).Execute();
+			CMD(b).CommandText(
+				L"DELETE FROM [StoredBase].[dbo].[TubesTable] WHERE Date_Time<?"
+				).Param(tme).Execute();
 
-		CMD(b).CommandText(L"SELECT count([Date_Time]) as C FROM [StoredBase].[dbo].[TubeTable]").GetValue(L"C", count);
-		return;
+			CMD(b).CommandText(L"SELECT count([Date_Time]) as C FROM [StoredBase].[dbo].[TubesTable]").GetValue(L"C", count);
+			return;
+		}
+		if(count > 0)
+		{
+			++count;
+		}
+		else
+		{
+			CMD(b).CommandText(L"SELECT count([Date_Time]) as C FROM [StoredBase].[dbo].[TubesTable]").GetValue(L"C", count);
+		}
 	}
-	if(count > 0)
-	{
-		++count;
-	}
-	else
-	{
-		CMD(b).CommandText(L"SELECT count([Date_Time]) as C FROM [StoredBase].[dbo].[TubeTable]").GetValue(L"C", count);
-	}
-}
 #else	
 #pragma message("Проверить удаление при заполнении базы данных")
-void DeleteLast::operator()(CBase &b, wchar_t *path_, wchar_t *offsPath_)
-{
-	path = path_;
-	offsPath= offsPath_;
-	base = &b;
-	/*
-	BOOL WINAPI GetDiskFreeSpaceEx(
-  _In_opt_  LPCTSTR         lpDirectoryName,
-  _Out_opt_ PULARGE_INTEGER lpFreeBytesAvailable,
-  _Out_opt_ PULARGE_INTEGER lpTotalNumberOfBytes,
-  _Out_opt_ PULARGE_INTEGER lpTotalNumberOfFreeBytes
-);
-*/
-	ULARGE_INTEGER freeBytesAvailable, totalNumberOfBytes, totalNumberOfFreeBytes;
-
-	wchar_t path[1024];
-	GetModuleFileName(0, path, dimention_of(path));
-	path[2] = 0;
-
-	bool diskSizeOk = 0 != GetDiskFreeSpaceEx(path, &freeBytesAvailable, &totalNumberOfBytes, &totalNumberOfFreeBytes);
-
-	unsigned countGb = freeBytesAvailable.QuadPart / 1024 * 1024 * 1024;
-
-	if(count > 10000 || (diskSizeOk && (countGb < 200))) //меньше 10 гиго байт
+	void DeleteLast::operator()(CBase &b, wchar_t *path_, wchar_t *offsPath_)
 	{
-		COleDateTime tme;
-		CMD(b).CommandText(
-			L"SELECT max([Date_Time]) as TME FROM [StoredBase].[dbo].[TubeTable]"\
-			L"WHERE [Date_Time] "\
-			L"IN (SELECT TOP(10)[Date_Time] FROM [StoredBase].[dbo].[TubeTable] ORDER BY [Date_Time] ASC)"
-			).GetValue(L"TME", tme);
-		Select<TubesTable>(b).op<Date_Time>(L"<", tme).ExecuteLoop<__list__>(*this);
+		path = path_;
+		offsPath= offsPath_;
+		base = &b;
+		/*
+		BOOL WINAPI GetDiskFreeSpaceEx(
+		_In_opt_  LPCTSTR         lpDirectoryName,
+		_Out_opt_ PULARGE_INTEGER lpFreeBytesAvailable,
+		_Out_opt_ PULARGE_INTEGER lpTotalNumberOfBytes,
+		_Out_opt_ PULARGE_INTEGER lpTotalNumberOfFreeBytes
+		);
+		*/
+		ULARGE_INTEGER freeBytesAvailable, totalNumberOfBytes, totalNumberOfFreeBytes;
 
-		CMD(b).CommandText(
-			L"DELETE FROM [StoredBase].[dbo].[TubeTable] WHERE Date_Time<?"
-			).Param(tme).Execute();
+		wchar_t path[1024];
+		GetModuleFileName(0, path, dimention_of(path));
+		path[2] = 0;
 
-		CMD(b).CommandText(
-			L"DELETE a"\
-			L" FROM [StoredBase].[dbo].[StoredMeshureTable] AS a"\
-			L" LEFT JOIN [StoredBase].[dbo].[TubeTable] AS b"\
-			L" ON a.ID = b.IDStoredMeshureTable"\
-			L" WHERE b.ID IS NULL"
-			).Execute();
+		bool diskSizeOk = 0 != GetDiskFreeSpaceEx(path, &freeBytesAvailable, &totalNumberOfBytes, &totalNumberOfFreeBytes);
 
-		CMD(b).CommandText(
-			L"DELETE a"\
-			L" FROM [StoredBase].[dbo].[ProtocolsTable] AS a"\
-			L" LEFT JOIN [StoredBase].[dbo].[TubesTable] AS b"\
-			L" ON a.ID = b.IDProtocolsTable"\
-			L" WHERE b.IDProtocolsTable IS NULL"
-			).Execute();
+		unsigned countGb = unsigned(freeBytesAvailable.QuadPart / (1024 * 1024 * 1024));
+		try
+		{
 
-		CMD(b).CommandText(L"SELECT count([Date_Time]) as C FROM [StoredBase].[dbo].[TubesTable]").GetValue(L"C", count);
-		return;
+			if(count > 10000 || (diskSizeOk && (countGb < 100))) //меньше 100 гиго байт
+			{
+				COleDateTime tme;
+				CMD(b).CommandText(
+					L"SELECT max([Date_Time]) as TME FROM [StoredBase].[dbo].[TubesTable]"\
+					L"WHERE [Date_Time] "\
+					L"IN (SELECT TOP(5)[Date_Time] FROM [StoredBase].[dbo].[TubesTable] ORDER BY [Date_Time] ASC)"
+					).GetValue(L"TME", tme);
+				Select<TubesTable>(b).op<Date_Time>(L"<", tme).ExecuteLoop<__list__>(*this);
+
+				CMD(b).CommandText(
+					L"DELETE FROM [StoredBase].[dbo].[TubesTable] WHERE Date_Time<?"
+					).Param(tme).Execute();
+
+				CMD(b).CommandText(
+					L"DELETE a"\
+					L" FROM [StoredBase].[dbo].[StoredMeshureTable] AS a"\
+					L" LEFT JOIN [StoredBase].[dbo].[TubesTable] AS b"\
+					L" ON a.ID = b.IDStoredMeshureTable"\
+					L" WHERE b.ID IS NULL"
+					).Execute();
+
+				CMD(b).CommandText(
+					L"DELETE a"\
+					L" FROM [StoredBase].[dbo].[ProtocolsTable] AS a"\
+					L" LEFT JOIN [StoredBase].[dbo].[TubesTable] AS b"\
+					L" ON a.ID = b.IDProtocolsTable"\
+					L" WHERE b.IDProtocolsTable IS NULL"
+					).Execute();
+
+				CMD(b).CommandText(L"SELECT count([Date_Time]) as C FROM [StoredBase].[dbo].[TubesTable]").GetValue(L"C", count);
+				return;
+			}
+			if(count > 0)
+			{
+				++count;
+			}
+			else
+			{
+				CMD(b).CommandText(L"SELECT count([Date_Time]) as C FROM [StoredBase].[dbo].[TubesTable]").GetValue(L"C", count);
+			}
+		}
+		catch(...){}
 	}
-	if(count > 0)
-	{
-		++count;
-	}
-	else
-	{
-		CMD(b).CommandText(L"SELECT count([Date_Time]) as C FROM [StoredBase].[dbo].[TubesTable]").GetValue(L"C", count);
-	}
-}
 #endif
 }
