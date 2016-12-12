df42000fecdad5f57acf6358d2f938853591bbcb
Andrew90 <drewan@rambler.ru>
2016-12-12 13:48:57 +0500

    VersionDiff

--- Units/Defectoscope/App/VersionDiff.cpp
+++ Units/Defectoscope/App/VersionDiff.cpp
@@ -7,7 +7,7 @@
 namespace Version
 {
 	static const int __magic_word__ = 0xcafe0000;
-	static const int __version__ = 2;
+	static const int __version__ = 3;
 
 	struct ScopeVelocity
 	{
@@ -22,8 +22,10 @@ namespace Version
 		ItemData<Thickness> &d = Singleton<ItemData<Thickness>>::Instance();
 		fwrite(d.scope_velocity, sizeof(d.scope_velocity), 1, f);
 
-		wchar_t *s = Singleton<ParametersTable>::Instance().items.get<NameParam>().value;
-		fwrite(s, sizeof(NameParam::type_value), 1, f);
+		//wchar_t *s = Singleton<ParametersTable>::Instance().items.get<NameParam>().value;
+		//fwrite(s, sizeof(NameParam::type_value), 1, f);
+		USPCIniFile &u = Singleton<USPCIniFile>::Instance();
+		fwrite(&u, sizeof(u), 1, f);
 	}
 
 	bool LoadFromFile(unsigned ver, FILE *f)
@@ -32,10 +34,23 @@ namespace Version
 		{
 			switch(ver & 0xffff)
 			{
+			case 3:
+				{
+					//ItemData<Thickness> &d = Singleton<ItemData<Thickness>>::Instance();
+					//fread(d.scope_velocity, sizeof(d.scope_velocity), 1, f);
+					double d;
+					fread(&d, sizeof(d), 1, f);
+
+					USPCIniFile u;// = Singleton<USPCIniFile>::Instance();
+	            	fwrite(&u, sizeof(u), 1, f);
+				}
+				return true;
 			case 2:
 				{
-					ItemData<Thickness> &d = Singleton<ItemData<Thickness>>::Instance();
-					fread(d.scope_velocity, sizeof(d.scope_velocity), 1, f);
+					//ItemData<Thickness> &d = Singleton<ItemData<Thickness>>::Instance();
+					double d;
+					//fread(d.scope_velocity, sizeof(d.scope_velocity), 1, f);
+					fread(&d, sizeof(d), 1, f);
 
 					//wchar_t *s = Singleton<ParametersTable>::Instance().items.get<NameParam>().value;
 					NameParam::type_value s;
