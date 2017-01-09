4b9ca04b51d021a68863c7c35e136d163e75af0a
AndreyNoutBook <andrey@uran-ndt.ru>
2017-01-09 15:38:59 +0500

    9

--- Units/Viewer/Viewer.cpp
+++ Units/Viewer/Viewer.cpp
@@ -2,10 +2,12 @@
 
 #include <windows.h>
 #include <commctrl.h>
+#include <ShellAPI.h>
 #include "Base\Base.hpp"
 #include "DiffApp/App.h"
 #include "window_tool/HookKey.h"
 #include "ProtocolTCP/InitTcp.h"
+#include "window_tool\Emptywindow.h"
 
 #if defined _M_IX86
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
@@ -17,19 +19,27 @@
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #endif
 
-wchar_t *typeWindow = L"LBT_Buran-5000_Defectoscope_160516";
+wchar_t *typeWindow = L"LBT_Buran-5000_Viewer_160812";
+COPYDATASTRUCT data = {0, 512 * sizeof(wchar_t), NULL};
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
 					   HINSTANCE hPrevInstance,
 					   LPTSTR    lpCmdLine,
 					   int       nCmdShow)
-{
+{	
 	CreateSemaphore(0, 0, 1, typeWindow);
 	if(GetLastError() == ERROR_ALREADY_EXISTS)
 	{
-		HWND h = FindWindow(typeWindow, 0);
+		HWND h = FindWindow(WindowClass<MainWindow>()(), 0);
 		SendMessage(h, WM_SYSCOMMAND, SC_RESTORE, 0);
-		SetForegroundWindow(h);
+		SetForegroundWindow(h);	
+		int num = 0;
+		LPWSTR *arg = CommandLineToArgvW(GetCommandLineW(), &num);
+		if(12 == wcslen(arg[1]))
+		{
+			data.lpData = arg[1];
+			SendMessage(h, WM_COPYDATA, (WPARAM)h, (LPARAM)&data);
+		}
 		return 0;
 	}
 	INITCOMMONCONTROLSEX *iccx = new INITCOMMONCONTROLSEX;
@@ -46,6 +56,16 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 	InitTcp initTcp;
 	
 	app.Init();
+
+	int num = 0;
+	LPWSTR *arg = CommandLineToArgvW(GetCommandLineW(), &num);
+	if(NULL != arg[1] && 12 == wcslen(arg[1]))
+	{
+		data.lpData = arg[1];
+		HWND h = app.MainWindowHWND();
+		SendMessage(h, WM_COPYDATA, (WPARAM)h, (LPARAM)&data);
+	}
+
 
 #ifdef THREAD_PRIORITY
 	SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS);
