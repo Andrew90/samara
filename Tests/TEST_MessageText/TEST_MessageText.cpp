ac125933ac1a033b0c53ef38d8aabae007814fb0
Andrew90 <drewan@rambler.ru>
2017-01-20 15:53:13 +0500

    test

--- Tests/TEST_MessageText/TEST_MessageText.cpp
+++ Tests/TEST_MessageText/TEST_MessageText.cpp
@@ -3,11 +3,28 @@
 
 #include "stdafx.h"
 #include <clocale>
-#include "SelectMessage.h"
 #include <Windows.h>
-#include "typelist.hpp"
-#include "DebugMess.h"
+#include <typeinfo>
+#include "D:\work\tmp\x\Samara170119\USPCSamara\Common\templates\typelist.hpp"
+#include "GenTlst.h"
+//#include "D:\work\tmp\x\Samara170119\USPCSamara\Tests\TEST_MessageText\MessageText\LabelMessage.h"
 
+#define XX(n) struct _##n{};
+
+
+XX(00)
+XX(01)
+XX(02)
+XX(03)
+
+
+
+
+#define TList1(_00)Tlst<_00,NullType>
+#define TList2(_00,_01)Tlst<_00, Tlst<_01,NullType>>
+#define TList3(_00,_01,_02)Tlst<_00,Tlst<_01,Tlst<_02,NullType>>>
+
+#define dprint
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -16,15 +33,22 @@ int _tmain(int argc, _TCHAR* argv[])
 	//Test();
 	int x[] = { 0, 5, -1 };
 		
-	char *s;
+	//char *s;
 	unsigned start = GetTickCount();
 //	for (int i = 0; i < dimention_of(x); ++i)
 	{
 		int t = 10000;
-		s = SelectMessage(x, t);
-		printf("%s  %d\n", s, t);
+	//	printf("%s  %d\n", s, t);
 	}
 
+	typedef TList3(_00,_01,_02) list0;
+
+	printf("%s\n", typeid(list0).name());
+
+	GenTlst gen;
+	gen(3);
+
+	printf("%s\n", gen.buf);
 	
 	getchar();
 	return 0;
